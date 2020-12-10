987b970eb36edf1d571b6c124ee5b7f2c17ac052
racar <rcarrascal@gmail.com>
2020-12-10 14:59:10 -0500

    fix job test and adding pry gem

--- spec/jobs/track_fedex_job_spec.rb
+++ spec/jobs/track_fedex_job_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe TrackFedexJob, type: :job do
   fixtures :carriers
   describe 'When track status from fedex api' do
-    subject { Fedex.find_by(tracking_number: '111111') }
+    let(:package) { Fedex.find_by(tracking_number: '111111') }
+    subject { package.status }
 
     before do
-      TrackFedexJob.perform_now(subject)
+      TrackFedexJob.perform_now(package)
     end
 
     it 'update status from package' do
-      expect(subject.status).to eq 'DELIVERED'
+      expect(subject).to eq 'DELIVERED'
     end
   end
 end
