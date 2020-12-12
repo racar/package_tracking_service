248da68a1a01c43a386638b8383cf6c565e4c6b7
racar <rcarrascal@gmail.com>
2020-12-12 18:10:11 -0500

    rabbitmq publisher

---
+++ app/events/publisher.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'dotenv/load'
+# Publish to rabbitMQ
+class Publisher
+  URL = ENV['CLOUDAMQP_URL']
+
+  def publish(msg)
+    raise NotImplementedError
+  end
+
+  def connection
+    @connection ||= Bunny.new URL
+  end
+
+  def channel
+    @channel ||= connection.create_channel
+  end
+
+  def queue
+    @queue ||= channel.queue(queue_name)
+  end
+
+  def exchange
+    channel.exchange('')
+  end
+
+  def queue_name
+    raise NotImplementedError
+  end
+end
