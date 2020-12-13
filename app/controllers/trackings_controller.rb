c25a14a0a448fd419df31ff44d450eea80d66012
racar <rcarrascal@gmail.com>
2020-12-13 02:20:37 -0500

    add OK render response

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,10 +1,9 @@
 class TrackingsController < ApplicationController
   def update
-    package = FedexService.find_by(tracking_number: tracking_id)
     if package
-      binding.pry
       begin
         TrackFedexJob.perform_now(package)
+        render_ok
       rescue StandardError => e
         render_unprocessable("Carrier searching error: #{e.message}")
       end
@@ -15,11 +14,19 @@ class TrackingsController < ApplicationController
 
   private
 
+  def package
+    @package ||= FedexService.find_by(tracking_number: tracking_id)
+  end
+
   def tracking_id
     params[:tracking_id]
   end
 
   def render_unprocessable(msg)
     render json: msg, status: :unprocessable_entity
+  end
+
+  def render_ok
+    render json: 'OK', status: :ok
   end
 end
